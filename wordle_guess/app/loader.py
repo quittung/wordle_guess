eb74f39d3aa793d7f167c0e9203be96120eedfc5
quittung <quittung@users.noreply.github.com>
2022-02-15 15:21:47 +0100

    made loading relative

--- wordle_guess/app/loader.py
+++ wordle_guess/app/loader.py
@@ -1,9 +1,14 @@
+from ntpath import join
 import pkgutil
 import json
 
 
 def load_str(file: str):
-    data = pkgutil.get_data("app.data", file)
+    module_path = __name__.split(".")
+    module_path[-1] = "data"
+    data_module = ".".join(module_path)
+
+    data = pkgutil.get_data(data_module, file)
     return data.decode("utf-8")
 
 def load_json(file: str):
