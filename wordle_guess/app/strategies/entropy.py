689e204afd72147a0ba09e5ba68fbfcabf8bdda2
quittung <quittung@users.noreply.github.com>
2022-02-16 18:05:57 +0100

    refactored entropy for better readability

--- wordle_guess/app/strategies/entropy.py
+++ wordle_guess/app/strategies/entropy.py
@@ -7,41 +7,62 @@ from .. import loader
 from ..wordle import get_pattern
 from . import strategy
 
-def normalize(counts: dict[str, int]):
-    counts_total = sum(counts.values())
-    return {e: counts[e] / counts_total for e in counts}
 
-def entropy_for_guess(words_frequency_normalized: dict[str, float], guess: str):
+
+def dictmap(function, data: dict):
+    """makes functions designed for lists work with dicts"""
+    return dict(zip(data.keys(), function(data.values())))
+
+def flexmap(func, data: list, pool: Pool = None, tracker = None):
+    """mapper that works with and without multithreading"""
+    if tracker:
+        data = tracker(data)
+    
+    if pool == None:
+        mapper = map
+    else:
+        if tracker:
+            mapper = partial(pool.map, chunksize = 250)
+        else:
+            mapper = pool.map
+    
+    return list(mapper(func, data))
+
+
+def normalize(values: list[float]) -> list[float]:
+    total = sum(values)
+    return [item / total for item in values]
+
+def get_information(rel_freq_list: list[float]):
+    """get the information from relative frequency for each list member"""
+    return [-math.log2(rf) for rf in rel_freq_list]
+
+def get_entropy(prob_list: list[float], info_list: list[float]):
+    """get entropy from list of probability of events and their information"""
+    return sum(p * i for p, i in zip(prob_list, info_list))
+
+
+def entropy_for_guess(words: dict[str, float], guess: str):
     # get pattern and do basic stats
-    solutions_pattern = {solution: get_pattern(guess, solution) for solution in words_frequency_normalized}
-    patterns_incidence = collections.Counter(solutions_pattern.values())
-    patterns_incidence_normalized = normalize(patterns_incidence)
+    pattern = {solution: get_pattern(guess, solution) for solution in words}
 
     # get information in bits describing how much a pattern narrows down the search space
-    patterns_information = {pattern: -math.log2(patterns_incidence_normalized[pattern]) for pattern in patterns_incidence_normalized}
+    pattern_counted = dictmap(normalize, collections.Counter(pattern.values()))
+    pattern_info = dictmap(get_information, pattern_counted)
 
     # get probability as sum of all word freqencies of a pattern
-    patterns_probability_normalized = {pattern: 0.0 for pattern in patterns_incidence}
-    for solution, pattern in solutions_pattern.items():
-        patterns_probability_normalized[pattern] += words_frequency_normalized[solution]
+    pattern_prob = {pattern: 0.0 for pattern in pattern_counted}
+    for solution, pattern in pattern.items():
+        pattern_prob[pattern] += words[solution]
 
     # calculates entropy as sum of probability * information for each element
-    entropy = sum([a * b for a, b in zip(patterns_information.values(), patterns_probability_normalized.values())])
-    return entropy
+    return get_entropy(pattern_prob.values(), pattern_info.values())
 
 def entropy_for_list(words_frequency: dict[str, float], pool: Pool = None, progress_tracker = None):
-    wordlist = list(words_frequency)
-    if progress_tracker: wordlist = progress_tracker(wordlist)
-    words_frequency_normalized = normalize(words_frequency)
+    words_frequency_normalized = dictmap(normalize, words_frequency)
 
-    if pool == None:
-        words_entropy = {word: entropy_for_guess(words_frequency_normalized, word) for word in wordlist}
-    else:
-        if progress_tracker:
-            entropy_list = pool.map(partial(entropy_for_guess, words_frequency_normalized), wordlist, chunksize=250)
-        else: 
-            entropy_list = pool.map(partial(entropy_for_guess, words_frequency_normalized), wordlist)
-        words_entropy = dict(zip(words_frequency.keys(), entropy_list))
+    words = words_frequency.keys()
+    words_entropy = dict(zip(words, flexmap(partial(entropy_for_guess, words_frequency_normalized), words, pool, progress_tracker)))
 
     words_entropy_sorted = dict(sorted(words_entropy.items(), key=lambda item: item[1], reverse=True))
     return words_entropy_sorted
