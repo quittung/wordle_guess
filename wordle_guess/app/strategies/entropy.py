6695daee074d6b769ca7f65a5e5ce942330095b8
quittung <quittung@users.noreply.github.com>
2022-02-19 22:59:48 +0100

    switched to using duck types for hinting

--- wordle_guess/app/strategies/entropy.py
+++ wordle_guess/app/strategies/entropy.py
@@ -2,6 +2,7 @@ import collections
 from functools import partial
 import math
 from multiprocessing.pool import Pool
+from typing import Callable, Mapping, Iterable
 
 from .. import loader
 from ..wordle import get_pattern
@@ -9,7 +10,7 @@ from . import strategy
 
 
 
-def optional_dict(function):
+def optional_dict(function: Callable[[Iterable], Iterable]) -> Callable[[Mapping], dict]:
     """makes functions designed for lists work with dicts"""
     def inner(data):
         if isinstance(data, dict):
@@ -20,21 +21,21 @@ def optional_dict(function):
     return inner
 
 @optional_dict
-def normalize(values: list[float]) -> list[float]:
+def normalize(values: Iterable[float]) -> Iterable[float]:
     total = sum(values)
     return [item / total for item in values]
 
 @optional_dict
-def get_information(rel_freq_list: list[float]):
+def get_information(rel_freq_list: Iterable[float]):
     """get the information from relative frequency for each list member"""
     return [-math.log2(rf) for rf in rel_freq_list]
 
-def get_entropy(prob_list: list[float], info_list: list[float]):
+def get_entropy(prob_list: Iterable[float], info_list: Iterable[float]):
     """get entropy from list of probability of events and their information"""
     return sum(p * i for p, i in zip(prob_list, info_list))
 
 
-def entropy_for_guess(words: dict[str, float], guess: str):
+def entropy_for_guess(words: Mapping[str, float], guess: str):
     # get pattern and do basic stats
     pattern = {solution: get_pattern(guess, solution) for solution in words}
 
